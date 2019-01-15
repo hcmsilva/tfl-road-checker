9ffa6b8dd34a6228cbf6f1fc29622891b689cd35
Henrique Silva <henriquecms@gmail.com>
2019-01-15 20:24:08 +0000

    first Models

---
+++ app/src/main/java/com/demos/henrique/tflroadchecker/model/ErrorStatus.java
@@ -0,0 +1,18 @@
+package com.demos.henrique.tflroadchecker.model;
+
+public class ErrorStatus implements Status {
+    @Override
+    public String getDisplayName() {
+        return null;
+    }
+
+    @Override
+    public String getStatus() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/demos/henrique/tflroadchecker/model/Status.java
@@ -0,0 +1,9 @@
+package com.demos.henrique.tflroadchecker.model;
+
+public interface Status {
+    String getDisplayName();
+
+    String getStatus();
+
+    String getDescription();
+}
